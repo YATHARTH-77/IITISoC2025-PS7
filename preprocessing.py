4e21cb47899521fd933dd0a4503cf99a3faeac22
Samay107 <samaymulchandani24@gmail.com>
2025-06-23 15:08:25 +0530

    Add files via upload

---
+++ preprocessing.py
@@ -0,0 +1,52 @@
+'''
+main features in preprocessing
+inverting(not that useful)
+rescaling
+binarization
+noise reduction(dilation, erosion, morphologyEx, median blur) 
+rotating
+removing borders
+'''
+import cv2 as cv
+import numpy as np
+import pytesseract as pyt
+import os
+def preprocess_image(image_path):
+
+    # Load the image
+    img = cv.imread(image_path)
+    if img is None:
+        raise ValueError("Image not found or unable to load.")
+
+    # grayscaling
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # binarization
+    threshold,thresh=cv.threshold(gray, 205, 225, cv.THRESH_BINARY)
+    def remove_noise(image):
+        kernel= np.ones((1, 1), np.uint8)
+        image=cv.dilate(image, kernel, iterations=1)
+        kernel = np.ones((1, 1), np.uint8)
+        image=cv.erode(image, kernel, iterations=1)
+        image=cv.morphologyEx(image, cv.MORPH_CLOSE, kernel)
+        image=cv.medianBlur(image, 3)
+        return image
+    im=remove_noise(thresh)
+    return im
+
+def process_folder(input_dir, output_dir,):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for filename in os.listdir(input_dir):
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            path = os.path.join(input_dir, filename)
+            processed = preprocess_image(path) 
+            out_path = os.path.join(output_dir, filename)
+            cv.imwrite(out_path, processed) 
+
+def display(image, title='Image'):
+    cv.imshow(title, image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()    
+
+process_folder('Datasets\Chinese\chinese dataset\PO0202-1-中文OCR数据样例\image\\nature\\advertising bord','Processed datasets\Chinese')
