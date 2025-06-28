338edb4afd6a638b56ba1fabbadd8799b2ba7056
Samay107 <samaymulchandani24@gmail.com>
2025-06-28 16:35:53 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -15,30 +15,30 @@ def preprocess_image(image_path):
     sharpen_amount = 1.7 if scale < 600 else 2.5
     sharpen_subtract = 0.7 if scale < 600 else 1.5
     morph_kernel_size = (1, 1)
+    bet= 200 if scale < 600 else 220
 
-    # 1. Convert to grayscale (makes a clear difference visually)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    # 2. Remove background noise while keeping edges
+    # 2. Remove background noise 
     blur = cv.medianBlur(gray, median_k)
 
     # 3. Contrast enhancement (visibly darkens text, brightens background)
-    
+    norm= cv.normalize(blur, None, alpha=0, beta=bet, norm_type=cv.NORM_MINMAX)
 
     # 4. Sharpen text edges
-    sharpened = cv.addWeighted(gray, sharpen_amount, blur, -sharpen_subtract, 0)
+    sharpened = cv.addWeighted(gray, sharpen_amount, norm, -sharpen_subtract, 0)
 
-    # 5. Morphological clean-up to reduce noise dots
+    #noise and dots cleaning
     kernel = np.ones(morph_kernel_size, np.uint8)
     morph = cv.morphologyEx(sharpened, cv.MORPH_OPEN, kernel, iterations=1)
     morph = cv.morphologyEx(morph, cv.MORPH_CLOSE, kernel, iterations=1)
 
-    # 6. Adaptive threshold for high visual contrast
+     #adapive threshholding
     if scale>600 : 
-        processed = cv.adaptiveThreshold(morph, 255, cv.ADAPTIVE_THRESH_MEAN_C,
+        processed = cv.adaptiveThreshold(morph, 235, cv.ADAPTIVE_THRESH_MEAN_C,
                                      cv.THRESH_BINARY, 23, 27)
     else:
-        processed = cv.adaptiveThreshold(morph, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C,
+        processed = cv.adaptiveThreshold(morph, 205, cv.ADAPTIVE_THRESH_GAUSSIAN_C,
                                      cv.THRESH_BINARY, 7, 7)
     
     return processed
@@ -63,8 +63,8 @@ def process_folder(input_dir, output_dir):
             except Exception as e:
                 print(f"Error processing {filename}: {str(e)}")
 
-image_path = 'D:\Coding\Datacluster Hindi Text Data (97).jpg'
+image_path = 'D:\Coding\WhatsApp Image 2025-06-27 at 11.56.42_ae6495e8.jpg'
 processed = preprocess_image(image_path)
 im = cv.imread(image_path)
 display_comparison(im, processed)
-print(pyt.image_to_string(processed, lang='hin'))
+print(pyt.image_to_string(processed, lang='eng'))
